d72f8f9e954e6654e7aeabc4177bc80facec4030
Dragan Glumac <dragan.glumac@gmail.com>
2015-02-17 12:42:47 +0000

    Adding makefile etc.

---
+++ my-curses/src/main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  17/02/15 12:41:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dragan Glumac (draganglumac), dragan.glumac@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <ncurses.h>
+#include <pthread.h>
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
